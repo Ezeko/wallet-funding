012cb39abbce25111d72551a6f3099a2539ef520
Ezekiel Adejobi <ezeko2017@gmail.com>
2020-08-17 11:23:56 +0100

    feat
    complete credit backend

--- connect/connect.php
+++ connect/connect.php
@@ -10,10 +10,4 @@
     //connection script
     $connected = mysqli_connect( $servername, $user, $userpwd, $dbname);
 
-    if ($connected) {
-        //echo ('connected to ' . $dbname);
-    } else {
-        die('failed to connect '. mysqli_connect_error());
-    }
-
 ?>
--- credit_user.php
+++ credit_user.php
@@ -11,10 +11,36 @@
         $amount = check_input($data);
         $user = check_input($_POST['username']);
 
-        include_once('./connect/connect.php');
+        include_once('./connect/connect.php'); // include connection script
 
         //check database
-        $sql = "SELECT * FROM `wallet-funding` WHERE username = '$user' ";
-    }
+        $sql = "SELECT * FROM `users` WHERE username = '$user' ";
+        $query = mysqli_query( $connected, $sql) or die(mysqli_error($connected));
 
+        if ( $query ) {
+            //user is a registered user 
+            //continue processing
+            $oldAmount = mysqli_fetch_array($query)['amount'];
+
+            $newAmount = $amount + $oldAmount;
+
+            //update database
+
+            $sql = "UPDATE `users` set amount = '$newAmount' WHERE username = '$user' ";
+
+            $query = mysqli_query($connected, $sql) or die(mysqli_error( $connected ));
+
+            if ( $query ) {
+                echo "<script> alert('$amount has been added to $user\'s wallet '); <window class='credit_user.php'></window> </script>";
+            } else {
+                echo "<script> alert('$user wallet cannot be fund at the moment!!!'); <window class='credit_user.php'></window> </script>";
+            }
+
+            
+
+        } else {
+            echo "<script> alert('$user is not registered'); <window class='credit_user.php'></window> </script>";
+        }
+    }
+?>
 
