7edde780134ce564b74075c8c43a3d76c7bdbe60
Ezekiel Adejobi <ezeko2017@gmail.com>
2020-08-17 13:00:14 +0100

    add credit and debit feature

--- credit_user.php
+++ credit_user.php
@@ -1,5 +1,8 @@
 <?php
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){ //check method of sending request
+        include_once('./connect/connect.php'); // include connection script
+        
         function check_input ( $input ) {
             trim($input); //remove extra spaces
             stripslashes( $input); //strip slashes
@@ -10,37 +13,80 @@
         $data = $_POST['amount'];
         $amount = check_input($data);
         $user = check_input($_POST['username']);
+        
 
-        include_once('./connect/connect.php'); // include connection script
 
         //check database
         $sql = "SELECT * FROM `users` WHERE username = '$user' ";
         $query = mysqli_query( $connected, $sql) or die(mysqli_error($connected));
+        $queryArray = mysqli_fetch_array($query);
 
-        if ( $query ) {
+        if ( count($queryArray) > 0 ) {
             //user is a registered user 
             //continue processing
-            $oldAmount = mysqli_fetch_array($query)['amount'];
+            $oldAmount = $queryArray['amount'];
 
-            $newAmount = $amount + $oldAmount;
-
+            $newAmount = $oldAmount + $amount;
+            
             //update database
 
-            $sql = "UPDATE `users` set amount = '$newAmount' WHERE username = '$user' ";
+            $sql = "UPDATE `users` SET `amount` = $newAmount WHERE `username` = '$user' ";
 
-            $query = mysqli_query($connected, $sql) or die(mysqli_error( $connected ));
-
+            $query = mysqli_query($connected, $sql) or die(mysqli_error($connected));
+            
             if ( $query ) {
-                echo "<script> alert('$amount has been added to $user\'s wallet '); <window class='credit_user.php'></window> </script>";
+                
+                echo "<script> alert('₦$amount has been added to $user\'s wallet '); window.location.replace('credit_user.php');</script>";
             } else {
-                echo "<script> alert('$user wallet cannot be fund at the moment!!!'); <window class='credit_user.php'></window> </script>";
+                echo "<script> alert('$user wallet cannot be funded at the moment!!!'); window.location.replace('credit_user.php'); </script>";
             }
 
             
 
         } else {
-            echo "<script> alert('$user is not registered'); <window class='credit_user.php'></window> </script>";
+            echo "<script> alert('$user is not registered'); window.location.replace('credit_user.php'); </script>";
         }
     }
 ?>
 
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="css/credit.css">
+
+
+<div class="container">
+	<div class="row">
+        <h2>Credit User!</h2>
+        <div class="col-md-6">
+        <a href="debit_user.php"> Debit Users Here </a>
+    </div>
+</div>
+
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" >
+    <div class="row form-group">
+        <div> 
+            <label for="amount"> AMOUNT</label>
+        </div>
+        <div class="input-group">
+            <span class="input-group-addon primary"><span class="glyphicon glyphicon-star"></span></span>
+            <input type="number" class="form-control" name="amount"
+            placeholder="amount to be credited" required>
+        </div>
+    </div>
+    <div class="row form-group">
+        <div>
+            <label for="username"> USERNAME</label>
+        </div>
+        <div class="input-group">
+            <span class="input-group-addon info"><span class="glyphicon glyphicon-info-sign"></span></span>
+            <input type="text" class="form-control" name="username"
+            placeholder="username" required>
+        </div>
+    </div>
+    <div>
+        <input type="submit" value="Credit" class="btn-success form-control" >
+    </div>
+    </form>
+</div>
+    
---
+++ debit_user.php
@@ -0,0 +1,94 @@
+<?php
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){ //check method of sending request
+        include_once('./connect/connect.php'); // include connection script
+        
+        function check_input ( $input ) {
+            trim($input); //remove extra spaces
+            stripslashes( $input); //strip slashes
+            htmlspecialchars($input); // encode special characters
+
+            return $input;
+        }
+        $data = $_POST['amount'];
+        $amount = check_input($data);
+        $user = check_input($_POST['username']);
+        
+
+
+        //check database
+        $sql = "SELECT * FROM `users` WHERE username = '$user' ";
+        $query = mysqli_query( $connected, $sql) or die(mysqli_error($connected));
+        $queryArray = mysqli_fetch_array($query);
+
+        if ( count($queryArray) > 0 ) {
+            //user is a registered user 
+            //continue processing
+            $oldAmount = $queryArray['amount'];
+            if ($amount > $oldAmount ) {
+                echo "<script> alert('Insufficient Fund!!!'); window.location.replace('debit_user.php'); </script>";
+                exit;
+            }
+            $newAmount = $oldAmount - $amount;
+            
+            //update database
+
+            $sql = "UPDATE `users` SET `amount` = $newAmount WHERE `username` = '$user' ";
+
+            $query = mysqli_query($connected, $sql) or die(mysqli_error($connected));
+            
+            if ( $query ) {
+                
+                echo "<script> alert('₦$amount has been removed to $user\'s wallet '); window.location.replace('debit_user.php');</script>";
+            } else {
+                echo "<script> alert('$user wallet cannot be debitted at the moment!!!'); window.location.replace('debit_user.php'); </script>";
+            }
+
+            
+
+        } else {
+            echo "<script> alert('$user is not registered'); window.location.replace('debit_user.php'); </script>";
+        }
+    }
+?>
+
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="css/credit.css">
+
+
+<div class="container">
+	<div class="row">
+        <h2>Debit User!</h2>
+        <div class="col-md-6">
+            <a href="credit_user.php"> Credit Users Here </a>
+        </div>
+    </div>
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" >
+    <div class="row form-group">
+        <div> 
+            <label for="amount"> AMOUNT</label>
+        </div>
+        <div class="input-group">
+            <span class="input-group-addon primary"><span class="glyphicon glyphicon-star"></span></span>
+            <input type="number" class="form-control" name="amount"
+            placeholder="amount to be credited" required>
+        </div>
+    </div>
+    <div class="row form-group">
+        <div>
+            <label for="username"> USERNAME</label>
+        </div>
+        <div class="input-group">
+            <span class="input-group-addon info"><span class="glyphicon glyphicon-info-sign"></span></span>
+            <input type="text" class="form-control" name="username"
+            placeholder="username" required>
+        </div>
+    </div>
+    <div>
+        <input type="submit" value="Credit" class="btn-info form-control" >
+    </div>
+    </form>
+</div>
+    
